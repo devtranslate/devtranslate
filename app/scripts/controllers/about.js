fa43ad5f8ef258045bc6ab2c12dcf78d1b994985
Larissa Mourullo <larismourullo@live.com>
2017-09-14 01:57:10 -0300

    :sparkles: Creates new structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name devtranslateApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the devtranslateApp
+ */
+angular.module('devtranslateApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name devtranslateApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the devtranslateApp
+ */
+angular.module('devtranslateApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
