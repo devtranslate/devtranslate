f5f9fff18a84633b1007ecfa77a2fd99ef189282
Larissa Abreu <larissa.a.abreu@hotmail.com>
2021-10-08 14:01:47 -0300

    feat: Insert infos about the text on logotipo

--- src/components/molecules/navbar/index.jsx
+++ src/components/molecules/navbar/index.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         href="/"
         title="Ir para a página inicial do Devtranslate"
         src="https://devtranslate.io/assets/images/logo-horizontal.svg"
-        alt="Duas caixas de texto. Uma na cor vermelha com um caracter oriental dentro e outra na cor amarela com um caracter ocidental dentro. As duas contém uma seta apontando uma para outra representando a tradução para ambos os idiomas."
+        alt="Duas caixas de texto. Uma na cor vermelha com um caracter oriental dentro e outra na cor amarela com um caracter ocidental dentro. As duas contém uma seta apontando uma para outra representando a tradução para ambos os idiomas. Ao lado dessas caixas de texto há o texto 'Devtranslate'."
       />
       <NavbarToggle active={active} onClick={() => setActive(!active)} />
       <NavbarMenu active={active}>
