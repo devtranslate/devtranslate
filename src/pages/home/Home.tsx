c9cf60f2676fcba51d2ae71b6ddcb7ad5e754049
Larissa Mourullo <larismourullo@gmail.com>
2022-03-17 20:46:46 -0300

    :construction: feat: adding background to pages

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,36 @@
+import styled from 'styled-components';
 import { HomeProps } from './Home.types';
 import { Footer } from '../../components/organisms/footer/Footer';
 import { Grid } from '../../components/atoms/grid/Grid';
 import { Header } from '../../components/organisms/header/Header';
+import { View } from '../../components/atoms/view/View';
 
 export const Home: React.FC<HomeProps> = () => {
   return (
-    <Grid
-      tag="main"
-      paddingTop={{ xs: 'm', l: 'xl' }}
-      paddingBottom={{ xs: 'm', l: 'xl' }}
-      paddingLeft={{ xs: 's' }}
-      paddingRight={{ xs: 's' }}
-      container
-    >
-      <Header />
-      <Footer />
-    </Grid>
+    <Background>
+      <GridUI
+        paddingTop={{ xs: 'm', l: 'xl' }}
+        paddingBottom={{ xs: 'm', l: 'xl' }}
+        paddingLeft={{ xs: 's' }}
+        paddingRight={{ xs: 's' }}
+        justifyContent={{ xs: 'space-between' }}
+        container
+      >
+        <Header />
+        <Footer />
+      </GridUI>
+    </Background>
   );
 };
+
+const Background = styled(View)`
+  background-image: url('/static/images/background.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const GridUI = styled(Grid)`
+  min-height: 100vh;
+  flex-direction: column;
+`;
--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,20 +1,36 @@
+import styled from 'styled-components';
 import { NotFoundProps } from './NotFound.types';
 import { Grid } from '../../components/atoms/grid/Grid';
 import { Footer } from '../../components/organisms/footer/Footer';
 import { Header } from '../../components/organisms/header/Header';
+import { View } from '../../components/atoms/view/View';
 
 export const NotFound: React.FC<NotFoundProps> = () => {
   return (
-    <Grid
-      tag="main"
-      paddingTop={{ xs: 'm', l: 'xl' }}
-      paddingBottom={{ xs: 'm', l: 'xl' }}
-      paddingLeft={{ xs: 's' }}
-      paddingRight={{ xs: 's' }}
-      container
-    >
-      <Header />
-      <Footer />
-    </Grid>
+    <Background>
+      <GridUI
+        paddingTop={{ xs: 'm', l: 'xl' }}
+        paddingBottom={{ xs: 'm', l: 'xl' }}
+        paddingLeft={{ xs: 's' }}
+        paddingRight={{ xs: 's' }}
+        justifyContent={{ xs: 'space-between' }}
+        container
+      >
+        <Header />
+        <Footer />
+      </GridUI>
+    </Background>
   );
 };
+
+const Background = styled(View)`
+  background-image: url('/static/images/background.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const GridUI = styled(Grid)`
+  min-height: 100vh;
+  flex-direction: column;
+`;
