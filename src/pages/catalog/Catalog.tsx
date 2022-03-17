1923fa29ed6f3da5a93b8791a2fa1e22b0f47459
Larissa Mourullo <larismourullo@gmail.com>
2022-03-16 23:31:32 -0300

    :construction: feat: developing API call on catalog page

--- src/pages/catalog/Catalog.tsx
+++ src/pages/catalog/Catalog.tsx
@@ -1,5 +1,28 @@
+import axios from 'axios';
+import queryString from 'query-string';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { CatalogProps } from './Catalog.types';
 
 export const Catalog: React.FC<CatalogProps> = () => {
-  return <>Catalog</>;
+  const [response, setResponse] = useState({ data: {}, loading: true, error: false });
+
+  const { search } = useLocation();
+  const parseQs = queryString.parse(search);
+  const { page, query } = parseQs;
+
+  useEffect(() => {
+    const params = { page, query, pageSize: 9 };
+
+    axios
+      .get(`https://api.devtranslate.io/translations?${queryString.stringify(params)}`)
+      .then(({ data }) => {
+        setResponse({ data, loading: false, error: false });
+      })
+      .catch(() => {
+        setResponse({ data: {}, loading: false, error: true });
+      });
+  }, [page, query]);
+
+  return <>Catalog </>;
 };
