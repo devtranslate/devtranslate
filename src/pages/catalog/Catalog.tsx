ae49c0c529105a54538d78016d9abf0dd7261d49
Larissa Mourullo <larismourullo@gmail.com>
2022-03-17 20:46:46 -0300

    :construction: feat: adding background to pages

--- src/pages/catalog/Catalog.tsx
+++ src/pages/catalog/Catalog.tsx
@@ -2,10 +2,12 @@ import axios from 'axios';
 import queryString from 'query-string';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 import { CatalogProps } from './Catalog.types';
 import { Grid } from '../../components/atoms/grid/Grid';
 import { Footer } from '../../components/organisms/footer/Footer';
 import { Header } from '../../components/organisms/header/Header';
+import { View } from '../../components/atoms/view/View';
 
 export const Catalog: React.FC<CatalogProps> = () => {
   const [response, setResponse] = useState({ data: {}, loading: true, error: false });
@@ -28,16 +30,30 @@ export const Catalog: React.FC<CatalogProps> = () => {
   }, [page, query]);
 
   return (
-    <Grid
-      tag="main"
-      paddingTop={{ xs: 'm', l: 'xl' }}
-      paddingBottom={{ xs: 'm', l: 'xl' }}
-      paddingLeft={{ xs: 's' }}
-      paddingRight={{ xs: 's' }}
-      container
-    >
-      <Header />
-      <Footer />
-    </Grid>
+    <Background>
+      <GridUI
+        paddingTop={{ xs: 'm', l: 'xl' }}
+        paddingBottom={{ xs: 'm', l: 'xl' }}
+        paddingLeft={{ xs: 's' }}
+        paddingRight={{ xs: 's' }}
+        justifyContent={{ xs: 'space-between' }}
+        container
+      >
+        <Header />
+        <Footer />
+      </GridUI>
+    </Background>
   );
 };
+
+const Background = styled(View)`
+  background-image: url('/static/images/background.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const GridUI = styled(Grid)`
+  min-height: 100vh;
+  flex-direction: column;
+`;
