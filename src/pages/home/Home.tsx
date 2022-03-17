5790ea27ade4501c52bf70b4d6f5ff8ec0be5de6
Larissa Mourullo <larismourullo@gmail.com>
2022-03-17 12:08:25 -0300

    :sparkles: feat: developing footer component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,18 @@
 import { HomeProps } from './Home.types';
+import { Footer } from '../../components/organisms/footer/Footer';
+import { Grid } from '../../components/atoms/grid/Grid';
 
 export const Home: React.FC<HomeProps> = () => {
-  return <>Home</>;
+  return (
+    <Grid
+      tag="main"
+      paddingTop={{ xs: 'm', l: 'xl' }}
+      paddingBottom={{ xs: 'm', l: 'xl' }}
+      paddingLeft={{ xs: 's' }}
+      paddingRight={{ xs: 's' }}
+      container
+    >
+      <Footer />
+    </Grid>
+  );
 };
