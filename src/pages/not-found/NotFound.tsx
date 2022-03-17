0c95551f3c307f9031bd702061bf19eef07c60c0
Larissa Mourullo <larismourullo@gmail.com>
2022-03-17 12:08:25 -0300

    :sparkles: feat: developing footer component

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,5 +1,18 @@
 import { NotFoundProps } from './NotFound.types';
+import { Grid } from '../../components/atoms/grid/Grid';
+import { Footer } from '../../components/organisms/footer/Footer';
 
 export const NotFound: React.FC<NotFoundProps> = () => {
-  return <>NotFound</>;
+  return (
+    <Grid
+      tag="main"
+      paddingTop={{ xs: 'm', l: 'xl' }}
+      paddingBottom={{ xs: 'm', l: 'xl' }}
+      paddingLeft={{ xs: 's' }}
+      paddingRight={{ xs: 's' }}
+      container
+    >
+      <Footer />
+    </Grid>
+  );
 };
